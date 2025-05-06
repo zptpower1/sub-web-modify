96005e03129ba9111db766d2691283da2170a6bc
lqyu <lqyu@relaper.com>
2025-05-06 12:36:01 +0800

    update subconvert domain

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -339,7 +339,7 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
-          "私有增强型后端【vless reality+hy1+hy2+Singbox】": "https://sub.imeibeauty.com",
+          "私有增强型后端【vless reality+hy1+hy2+Singbox】": "https://subconvert.v9.gg",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
           "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
